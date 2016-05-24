7babf6b469a09881c8b615a9353fdc093ca00695
Andrei Bechet <andrei.bechet@esrlabs.com>
2016-05-24 17:04:01 +0200

    refactor in tests: removed duplication

--- app/src/test/java/andreibechet/com/numbers/ParseNumberTest.java
+++ app/src/test/java/andreibechet/com/numbers/ParseNumberTest.java
@@ -6,39 +6,34 @@ import static org.junit.Assert.*;
 
 public class ParseNumberTest {
 
+    public static final Integer SOME_NUMBER = 140;
+    public static final Integer SOME_OTHER_NUMBER = 150;
+    public static final Integer ZERO = 0;
+    public static final Integer NEGATIVE = -2;
+
     @Test
     public void shouldComputeTheSection() {
-        Integer someNumber = 140;
-        ParseNumber parseNumber = new ParseNumber(someNumber);
         assertEquals("The session was not computed correctly",
                 ParseNumber.Session.Session1,
-                parseNumber.session);
+                new ParseNumber(SOME_NUMBER).session);
 
-        Integer someOtherNumber = 150;
-        ParseNumber parseAnotherNumber = new ParseNumber(someOtherNumber);
         assertEquals("The session was not computed correctly",
                 ParseNumber.Session.Session3,
-                parseAnotherNumber.session);
+                new ParseNumber(SOME_OTHER_NUMBER).session);
 
-        Integer zero = 0;
-        ParseNumber parseZero = new ParseNumber(zero);
         assertEquals("The session was not computed correctly",
                 ParseNumber.Session.Session1,
-                parseZero.session);
+                new ParseNumber(ZERO).session);
 
-        Integer negative = -2;
-        ParseNumber parseNegative = new ParseNumber(negative);
         assertEquals("The session was not computed correctly",
                 ParseNumber.Session.Session3,
-                parseNegative.session);
+                new ParseNumber(NEGATIVE).session);
     }
 
     @Test
     public void shouldComputeTheItemNumber() {
-        Integer someNumber = 140;
-        ParseNumber parseNumber = new ParseNumber(someNumber);
         assertEquals("The session was not computed correctly",
                 ParseNumber.ItemNumber.Item4,
-                parseNumber.itemNumber);
+                new ParseNumber(SOME_NUMBER).itemNumber);
     }
 }
